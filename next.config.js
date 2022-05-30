5cdc784705825f6f8011f53fc133627dd16c92c2
Alex Bashuk <bashuk@fb.com>
2022-05-30 21:35:17 +0100

    Implement /donate using redirects

--- next.config.js
+++ next.config.js
@@ -18,5 +18,19 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/donate',
+        destination: '/c4b84f78ac5d4ded9ab380f1dfcbca8a#fabdc8b2aa904351a543b0b8a867a477',
+        permanent: false,
+      },
+      {
+        source: '/en/donate',
+        destination: '/0fc82c3870dd455da34242bd2d692cad#c3b6093e5509494aa2748d08a04ce277',
+        permanent: false,
+      },
+    ]
+  },
 })
